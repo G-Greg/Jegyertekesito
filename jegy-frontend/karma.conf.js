7be79cabd190021d818d6e97e5794ef181e5df20
Gréts Gergő <grets.gergo@gmail.com>
2022-10-11 10:50:59 +0200

    rename

--- jegy-frontend/karma.conf.js
+++ jegy-frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/jegyertekesito'),
+      dir: require('path').join(__dirname, './coverage/jegy-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- jegy-frontend/src/app/app.component.spec.ts
+++ jegy-frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jegyertekesito'`, () => {
+  it(`should have as title 'jegy-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jegyertekesito');
+    expect(app.title).toEqual('jegy-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('jegyertekesito app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('jegy-frontend app is running!');
   });
 });
